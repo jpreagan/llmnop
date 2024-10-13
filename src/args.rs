9b4adeab221766a82e155282c8c18bb995832866
James Reagan <james@jpreagan.com>
2024-10-12 15:15:09 -1000

    Use environment variables for api key and base url

--- src/args.rs
+++ src/args.rs
@@ -3,14 +3,6 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    /// API key for the inference server
-    #[arg(short = 'k', long)]
-    pub api_key: String,
-
-    /// Base URL for the inference server
-    #[arg(short = 'u', long)]
-    pub base_url: String,
-
     /// Model to benchmark
     #[arg(short, long)]
     pub model: String,
@@ -31,11 +23,11 @@ pub struct Args {
     #[arg(long, default_value = "150")]
     pub stddev_input_tokens: u32,
 
-    /// Mean number of tokens to generate from each LLM request
+    /// Mean number of tokens to generate from each request
     #[arg(long, default_value = "150")]
     pub mean_output_tokens: u32,
 
-    /// Standard deviation on the number of tokens to generate per LLM request
+    /// Standard deviation on the number of tokens to generate per request
     #[arg(long, default_value = "10")]
     pub stddev_output_tokens: u32,
 }
